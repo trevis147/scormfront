70ae806ebeeb7fee208a7a9a61b142873a3341a7
Trevisan <rafael.trevisan@vylex.com.br>
2023-06-09 15:25:21 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ScormProvider from "react-scorm-provider";
 import Player from "./player";
 
 const App = () => {
+  const [startUrl, setStartUrl] = useState("");
+  const [sco, setSco] = useState(null);
+
+  useEffect(() => {
+    window.API = 'https://scorm2.vercel.app/imsmanifest.xm'
+    
+    fetch("https://scorm2.vercel.app/imsmanifest.xml")
+      .then((response) => response.text())
+      .then((manifestContent) => {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(manifestContent, "text/xml");
+        const resources = xmlDoc.getElementsByTagName("resource");
+        debugger
+        const indexResource = resources[0]; 
+
+        if (indexResource) {
+          const indexUrl = indexResource.getAttribute("href");
+          const manifestBaseUrl = "https://scorm2.vercel.app/"; 
+          const indexFullPath = new URL(indexUrl, manifestBaseUrl).toString();
+          setStartUrl(indexFullPath);
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao carregar o manifest:", error);
+      });
+  }, []);
+
+  const handleScoInitialized = (sco) => {
+    debugger
+    setSco(sco);
+  };
+
   return (
-    <ScormProvider version="1.2" debug={true}>
+    <>
       <h1>We've got a connection just by including ScormProvider!</h1>
       <p>
         I'm a child with no access to the ScormProvider's props. But the
         ScoLearner component is enhanced with withScorm()!
       </p>
-      <Player />
-    </ScormProvider>
+      <ScormProvider
+        manifestUrl={"https://scorm2.vercel.app/imsmanifest.xml"}
+        onScoInitialized={handleScoInitialized}
+      >
+        <Player sco={sco} startUrl={startUrl} />
+      </ScormProvider>
+    </>
   );
 };
 
--- src/player.jsx
+++ src/player.jsx
@@ -1,35 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { withScorm } from "react-scorm-provider";
 
-class Player extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: []
-    };
-  }
+const Player = ({ sco, startUrl }) => {
+  useEffect(() => {
+    console.log(sco)
+    if (sco && sco.apiConnected) {
+      const api = sco.getAPI();
+      const success = api.Initialize("");
 
-  render() {
-    return (
-      <div>
-        <iframe
-          src="https://scorm-rose.vercel.app/genially.html"
-          height="300"
-          width="100%"
-        />
-        <br />
-        <button
-          onClick={() => {
-            console.log(this.props.sco.get("cmi.core.lesson_location "));
-          }}
-        >
-          Click Me
-        </button>
-        <br />
-      
-      </div>
-    );
-  }
-}
+      if (success) {
+        const lessonLocation = api.GetValue("cmi.core.lesson_location");
+        console.log("Lesson Location:", lessonLocation);
+      } else {
+        console.error("Failed to initialize the SCORM API");
+      }
+    }
+  }, [sco]);
+
+  return (
+    <div>
+      <h2>Player</h2>
+      <iframe src={startUrl} title="SCORM Player" width="800" height="600" />
+      <p>Title: {sco && sco.get("cmi.core.lesson_location")}</p>
+    </div>
+  );
+};
 
 export default withScorm()(Player);
